36fb8eff364b5b0ec73182fa73336b1fcdb7ec51
txaty <xtianae@connect.ust.hk>
2022-08-15 09:39:24 +0800

    update chaincode

--- chaincode/smart_contract.go
+++ chaincode/smart_contract.go
@@ -1,7 +1,6 @@
 package chaincode
 
 import (
-	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -21,80 +20,52 @@ type SmartContract struct {
 // Transaction describes basic details of what makes up a simple transaction.
 // Insert struct field in alphabetic order => to achieve determinism across languages.
 // golang keeps the order when marshal to json but doesn't order automatically.
-// Binding: the binding of personal information hash and certificate information hash
-// i.e. (person_info_hash || cert_into_hash)
-// Timestamp: the timestamp of the transaction
-type Transaction struct {
-	Binding   string `json:"Binding"`
-	Timestamp int64  `json:"Timestamp"`
-}
+type Transaction []byte
 
 // InitLedger adds a base set of transactions to the ledger.
 func (s *SmartContract) InitLedger(tci contractapi.TransactionContextInterface) error {
 	transactions := []Transaction{
-		{
-			Binding:   "test_binding",
-			Timestamp: 0,
-		},
+		[]byte("test"),
 	}
-	sha256Func := sha256.New()
 	for _, tx := range transactions {
-		// compose transaction key: SHA256 hash of the transaction in JSON format
-		txJSON, err := json.Marshal(tx)
-		if err != nil {
-			return err
-		}
-		sha256Func.Write(txJSON)
-		txKey := hex.EncodeToString(sha256Func.Sum(nil))
-		err = tci.GetStub().PutState(txKey, txJSON)
-		if err != nil {
+		txKey := hex.EncodeToString(tx)
+		if err := tci.GetStub().PutState(txKey, tx); err != nil {
 			return fmt.Errorf("failed to put to world state: %v", err)
 		}
-		sha256Func.Reset()
 	}
 	return nil
 }
 
 // CreateTX issues a new transaction to the world state with given details.
-func (s *SmartContract) CreateTX(ctx contractapi.TransactionContextInterface, binding string, timestamp int64) (string, error) {
-	tx := Transaction{
-		Binding:   binding,
-		Timestamp: timestamp,
-	}
-	// compose transaction key
-	txJSON, err := json.Marshal(tx)
+func (s *SmartContract) CreateTX(ctx contractapi.TransactionContextInterface, hash string) (string, error) {
+	tx, err := hex.DecodeString(hash)
 	if err != nil {
 		return "", err
 	}
-	hash := sha256.New()
-	hash.Write(txJSON)
-	txKey := hex.EncodeToString(hash.Sum(nil))
-	exists, err := s.TXExists(ctx, txKey)
+	exists, err := s.TXExists(ctx, hash)
 	if err != nil {
 		return "", err
 	}
 	if exists {
-		return "", fmt.Errorf("the transaction %s already exists", txKey)
+		return "", fmt.Errorf("the transaction %s already exists", hash)
 	}
-	return txKey, ctx.GetStub().PutState(txKey, txJSON)
+	return hash, ctx.GetStub().PutState(hash, tx)
 }
 
-// ReadTX returns the transaction stored in the world state with given id.
-func (s *SmartContract) ReadTX(ctx contractapi.TransactionContextInterface, id string) (*Transaction, error) {
-	txJSON, err := ctx.GetStub().GetState(id)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read from world state: %v", err)
-	}
-	if txJSON == nil {
-		return nil, fmt.Errorf("the transaction %s does not exist", id)
-	}
-	tx := new(Transaction)
-	err = json.Unmarshal(txJSON, tx)
-	if err != nil {
-		return nil, err
-	}
-	return tx, nil
-}
+//// ReadTX returns the transaction stored in the world state with given id.
+//func (s *SmartContract) ReadTX(ctx contractapi.TransactionContextInterface, id string) (*Transaction, error) {
+//	tx, err := ctx.GetStub().GetState(id)
+//	if err != nil {
+//		return nil, fmt.Errorf("failed to read from world state: %v", err)
+//	}
+//	if tx == nil {
+//		return nil, fmt.Errorf("the transaction %s does not exist", id)
+//	}
+//	if err != nil {
+//		return nil, err
+//	}
+//	return tx, nil
+//}
 
 // DeleteTX deletes a given transaction from the world state.
 func (s *SmartContract) DeleteTX(ctx contractapi.TransactionContextInterface, id string) error {
